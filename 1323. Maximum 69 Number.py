bb6927167dc9f1932b83d680a533e36ebca5b707
Moth <moth.feedback@gmail.com>
2023-05-20 09:36:32 +0300

    new solutions

---
+++ 1323. Maximum 69 Number.py
@@ -0,0 +1,9 @@
+# 1323. Maximum 69 Number
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num = str(num)
+        if '6' not in num:
+            return int(num)
+        i = num.find('6')
+        return int(num[:i] + '9' + num[i+1:])
