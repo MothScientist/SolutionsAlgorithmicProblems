afcee681dfc266d09c8ea839ad0b6443b49de711
Moth <moth.feedback@gmail.com>
2023-09-29 02:35:41 +0300

    update

--- 136. Single Number.py
+++
@@ -1,7 +0,0 @@
-# 136. Single Number
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        key = 0
-        for i in nums:
-            key = key ^ i
-        return key
--- 1431. Kids With the Greatest Number of Candies.py
+++
@@ -1,8 +0,0 @@
-# 1431. Kids With the Greatest Number of Candies
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        answer = [False for x in range(len(candies))]
-        for i in range(len(candies)):
-            if candies[i] + extraCandies >= max(candies):
-                answer[i] = True
-        return answer
