335b2aa9d0356aeca59d3d728cb437654fddadf6
Moth <moth.feedback@gmail.com>
2023-12-08 00:52:32 +0300

    update

--- LeetCode (en comments)/13. Roman to Integer.cpp
+++ LeetCode (en comments)/13. Roman to Integer.cpp
@@ -4,31 +4,27 @@
 class Solution {
 public:
     int romanToInt(std::string s) {
-        std::unordered_map<char, int> math = {
+        std::unordered_map<char, int> d = {
             {'I', 1}, {'V', 5}, {'X', 10},
             {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
         };
-        
-        int answer = 0;
-        int digit = math[s[0]];
-        
+        int res = 0;
+        int diff = d[s[0]];
         for (int i = 1; i < s.length(); i++) {
-            if (math[s[i]] == math[s[i - 1]]) {
-                digit += math[s[i - 1]];
-            } else {
-                digit += math[s[i]];
-                if (math[s[i]] > math[s[i - 1]]) {
-                    answer += 2 * math[s[i]] - digit;
-                    digit = 0;
-                } else {
-                    answer += digit - math[s[i]];
-                    digit = math[s[i]];
-                }
-            }
+           if (d[s[i]] == d[s[i - 1]]){
+                diff += d[s[i]];
+           }
+           else if (d[s[i]] > d[s[i - 1]]){
+               res += d[s[i]] - diff;
+               diff = 0;
+           }
+           else if (d[s[i]] < d[s[i - 1]]){
+               res += diff;
+               diff = d[s[i]];
+           }
         }
-        
-        answer += digit;
-        return answer;
+        res += diff;
+        return res;
     }
 };
 
--- LeetCode (en comments)/13. Roman to Integer.py
+++ LeetCode (en comments)/13. Roman to Integer.py
@@ -1,18 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        math = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        answer = 0
-        digit = math.get(s[0])
+        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        res = 0
+        diff = d.get(s[0])
         for i in range(1, len(s)):
-            if math.get(s[i]) == math.get(s[i - 1]):
-                digit += math.get(s[i - 1])
-            else:
-                digit += math.get(s[i])
-                if math.get(s[i]) > math.get(s[i - 1]):
-                    answer += 2*math.get(s[i]) - digit
-                    digit = 0
-                else:
-                    answer += digit - math.get(s[i])
-                    digit = math.get(s[i])
-        answer += digit
-        return answer
+            if d.get(s[i]) == d.get(s[i-1]):
+                diff += d.get(s[i])
+            elif d.get(s[i]) > d.get(s[i-1]):
+                res += d.get(s[i]) - diff
+                diff = 0
+            elif d.get(s[i]) < d.get(s[i-1]):
+                res += diff
+                diff = d.get(s[i])
+        res += diff
+        return res
